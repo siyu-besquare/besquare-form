765017b2efdd1a8b5d67a926b83bed3fe20e68e9
Siyu <si.yu@besquare.com.my>
2022-07-09 20:27:37 +0800

    react template

---
+++ react-todo-app/src/App.tsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+import "./App.css";
+
+export default function App() {
+  const [name, setName] = React.useState<string>("");
+  const [age, setAge] = React.useState<string | undefined>("");
+  const [color, setColor] = React.useState<string | undefined>("red");
+
+  const changeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const changeAge = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAge(event.target.value);
+  };
+
+  const changeColor = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setColor(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(name, age, color);
+  };
+
+  return (
+    <div className="pa-16">
+      <form onSubmit={handleSubmit}>
+        <Input name="Name" value={name} onChange={changeName} />
+        <Input name="Age" value={age} onChange={changeAge} />
+        <Select name="Favourite Color" value={color} onChange={changeColor} />
+        <button type="submit" className="btn-primary mb-16">
+          Submit
+        </button>
+        <button className="btn-secondary">Clear</button>
+      </form>
+    </div>
+  );
+}
+
+type InputType = {
+  name: string;
+  value: string | undefined;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Input = ({ value, onChange, name }: InputType) => {
+  return (
+    <div className="mb-16">
+      <label>{name}</label>
+      <input value={value} onChange={onChange} />
+    </div>
+  );
+};
+
+type SelectType = {
+  name: string;
+  value: string | undefined;
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+};
+
+const Select = ({ value, onChange, name }: SelectType) => {
+  return (
+    <div className="mb-16">
+      <label>{name}</label>
+      <select value={value} onChange={onChange}>
+        <option value="red">Red</option>
+        <option value="blue">Blue</option>
+      </select>
+    </div>
+  );
+};
