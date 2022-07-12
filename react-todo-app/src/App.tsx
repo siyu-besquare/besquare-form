2c8cbf84137d2f287390338e7df50a9eaba400d0
Siyu <si.yu@besquare.com.my>
2022-07-12 09:35:57 +0800

    added gender radio button

--- react-todo-app/src/App.tsx
+++ react-todo-app/src/App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   const [name, setName] = React.useState<string>("");
   const [age, setAge] = React.useState<string | undefined>("");
   const [color, setColor] = React.useState<string | undefined>("red");
+  const [gender, setGender] = React.useState<string | undefined>("");
 
   const changeName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
@@ -18,9 +19,13 @@ export default function App() {
     setColor(event.target.value);
   };
 
+  const changeGender = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setGender(event.target.value);
+  };
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(name, age, color);
+    console.log(name, age, color, gender);
   };
 
   return (
@@ -28,6 +33,7 @@ export default function App() {
       <form onSubmit={handleSubmit}>
         <Input name="Name" value={name} onChange={changeName} />
         <Input name="Age" value={age} onChange={changeAge} />
+        <RadioInput name="Input" onChange={changeGender} />
         <Select name="Favourite Color" value={color} onChange={changeColor} />
         <button type="submit" className="btn-primary mb-16">
           Submit
@@ -53,6 +59,38 @@ const Input = ({ value, onChange, name }: InputType) => {
   );
 };
 
+type RadioType = {
+  name: string;
+  // value: string | undefined;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const RadioInput = ({ name, onChange }: RadioType) => {
+  return (
+    <div className="mb-16">
+      <label>{name}</label>
+      <br />
+      <input
+        type="radio"
+        name="gender"
+        value="Male"
+        onChange={onChange}
+        className="radioInput"
+      />
+      <label>Male</label>
+      <br />
+      <input
+        type="radio"
+        name="gender"
+        value="Female"
+        onChange={onChange}
+        className="radioInput"
+      />
+      <label>Female</label>
+    </div>
+  );
+};
+
 type SelectType = {
   name: string;
   value: string | undefined;
@@ -70,3 +108,15 @@ const Select = ({ value, onChange, name }: SelectType) => {
     </div>
   );
 };
+
+// class app extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <input type="radio" value="Male" name="gender" /> Male
+//         <input type="radio" value="Female" name="gender" /> Female
+//         <input type="radio" value="Other" name="gender" /> Other
+//       </div>
+//     );
+//   }
+// }
